d99e62c6bace4b2bbb25c9d00ca6ffc8f4ce0184
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-02-03 16:04:13 -0200

    create time component

---
+++ src/components/Time.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import BotaoGol from './BotaoGol'
+
+export default class Time extends React.Component {
+
+    render(){
+        return (
+            <div>
+                <h1>Vasco</h1>
+                <h2>0</h2>
+                <BotaoGol />
+            </div>
+        )
+    }
+}
