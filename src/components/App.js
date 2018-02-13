9dac45e52f6ab6c271df0cf603ba997957a0515c
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-02-13 15:35:30 -0200

    create const dados

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react'
 
+import PlacarContainer from './PlacarContainer'
+
+const dados = {
+    partida: {
+        estadio: 'Maracanã',
+        data: '24/02/2018 ',
+        horario: ' 17h00'
+    },
+    casa: {
+        nome: 'Flamengo'
+    },
+    visitante: {
+        nome: 'Santos'
+    }
+}
+
 export default class App extends React.Component {
     render(){
-        return <h1>Ola cirilo</h1>
+        return <PlacarContainer partida={dados.partida}
+                                casa={dados.casa}
+                                visitante={dados.visitante} />
     }
 }
