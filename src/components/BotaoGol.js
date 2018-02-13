afe9c8e7328e6ceb394f0a37edda14e0aee7ff8a
thiagofrancisquete <thiago.francisquete@gmail.com>
2018-02-13 15:34:24 -0200

    create method handleClick

--- src/components/BotaoGol.js
+++ src/components/BotaoGol.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 export default class BotaoGol extends React.Component {
 
+    handleClick(event){
+        event.preventDefault()
+        this.props.marcarGol()
+        
+    }
+
     render(){
         return (
-         <button>Gol </button>   
+         <button onClick={this.handleClick.bind(this)} >Gol </button>   
         )
     }
 }
