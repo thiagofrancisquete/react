73e2f1fd793a2dda059edfbe64b507edc338cda6
thiagofrancisquete <thiago.francisquete@gmail.com>
2019-09-10 18:30:51 -0300

    setState e remocao do serviceWorker

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>Initial file</h1>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Thiago'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ola {this.state.string} seu gostoso.</h1>
+        <button onClick={() => this.setState({ string: 'Arcanine' })}>
+          Change
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
