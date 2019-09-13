22c523d08015a021a23a6039c5658839ae262499
thiagofrancisquete <thiago.francisquete@gmail.com>
2019-09-13 18:08:39 -0300

    search-box component

--- monsters-rolodex/src/components/search-box/search-box.component.jsx
+++ monsters-rolodex/src/components/search-box/search-box.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './search-box.styles.css';
 
-export const SearchBox = props => (
+export const SearchBox = ({ placeholder, handleChange}) => (
   <input
-    className='search-box'
-    type='search'
-    placeholder='search monsters'
-    onChange={props.onSearchChange}
+    className="search-box"
+    type="search"
+    placeholder={placeholder}
+    onChange={handleChange} 
   />
 );
